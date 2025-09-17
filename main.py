4e54f09a0d2c66d4b38174f7b18c662c1a0555cd
anjanakannan5 <anjanakannan.ak2006@gmail.com>
2025-09-17 17:47:06 +0530

    new addition

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ def add(a,b):
     return a+b
 def sub(a,b):
     return a-b
+def sq(a):
+    return a*a
+
+def main():
+    a=10
+    b=5
+    print(add(a,b))
+    print(sub(a,b))
+    print(mul(a,b))
+    print(div(a,b))
+    print(sq(a,b))
+    
